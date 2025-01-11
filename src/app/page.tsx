21bfcc719bd0c216a711ee95e6eb3914a9a0eb00
uzairzia <uzair.governorsindh@gmail.com>
2025-01-11 08:22:33 +0500

    removed ' from the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+// import Header from "./components/header";
 import Image from "next/image";
 import profilePic from "@/app/uzair_pic.png"
 
@@ -9,7 +9,7 @@ export default function Home() {
       
       <div className="mainContainer-of-Home-Page">
         <div className="descriptionAboutMe">
-        Hi, <br /> <br /> I'm <span className="uzair-zia">Uzair Zia </span> from Karachi, Pakistan, with a Master's in Business Administration and 4+ years of experience in Human Resource Management. <br /><br />Passionate about web development and coding, I'm currently pursuing a course in Artificial Intelligence, Web 3.0, and the Metaverse at GIAIC, where I've mastered TypeScript and am now exploring Next.js.
+        Hi, <br /> <br /> I am <span className="uzair-zia">Uzair Zia </span> from Karachi, Pakistan, with a Masters in Business Administration and 4+ years of experience in Human Resource Management. <br /><br />Passionate about web development and coding, I am currently pursuing a course in Artificial Intelligence, Web 3.0, and the Metaverse at GIAIC, where I have mastered TypeScript and am now exploring Next.js.
         </div>
 
         <div className="homePageImage">
